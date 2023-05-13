5cee7ebfb3e8c2166a119aafe4265e7ad791bc7f
avalentincic <andraz.valentincic@student.um.si>
2023-05-13 18:07:25 +0200

    add brute force solution

--- brute_force.cpp
+++ brute_force.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <vector>
 
 using namespace std;
+
+void findInsertedIndices(const std::string& original, const std::string& modified, std::vector<int> &indices) {
+
+    int i = 0;  // Index for the original string
+    int j = 0;  // Index for the modified string
+
+    while (i < original.length() && j < modified.length()) {
+        if (original[i] == modified[j]) {
+            // Characters match, move to the next indices
+            i++;
+            j++;
+        } else {
+            // Character was inserted in the modified string
+            indices.push_back(j+1);
+            j++;
+        }
+    }
+
+    // Any remaining characters in the modified string are considered inserted
+    while (j < modified.length()) {
+        indices.push_back(j+1);
+        j++;
+    }
+}
 
 int pickedCount(char buff[], int m){
     int count = 0;
@@ -12,12 +37,17 @@ int pickedCount(char buff[], int m){
     return count;
 }
 
-void printPatternUtil(const string str, char buff[], int i, int j, int n, int k, int m)
+void printPatternUtil(const string str, char buff[], int i, int j, int n, int k, int m, int &min, char solu[])
 {
     if (i == n)
     {
         buff[j] = '\0';
-        cout << buff << " " << pickedCount(buff, m) << "\n";
+        int picked = pickedCount(buff, m);
+        if (picked < min) {
+            min = picked;
+            strcpy(solu, buff);
+            //cout << buff << " " << picked << "\n";
+        }
         return;
     }
 
@@ -28,30 +58,34 @@ void printPatternUtil(const string str, char buff[], int i, int j, int n, int k,
             j++;
         }
         buff[j] = '\0';
-        cout << buff << " " << pickedCount(buff, m) << "\n";
+        int picked = pickedCount(buff, m);
+        if (picked < min) {
+            min = picked;
+            strcpy(solu, buff);
+            //cout << buff << " " << picked << "\n";
+        }
         return;
     }
-
     // Either put the character
     buff[j] = str[i];
-    printPatternUtil(str, buff, i + 1, j + 1, n, k, m);
+    printPatternUtil(str, buff, i + 1, j + 1, n, k, m, min, solu);
 
     // Or put 1
     buff[j] = '1';
     //buff[j + 1] = str[i];
-    printPatternUtil(str, buff, i, j + 1, n, k-1, m);
+    printPatternUtil(str, buff, i, j + 1, n, k-1, m, min, solu);
 }
 
-void printPattern(const string str, int n, int &k, int m)
+void printPattern(const string& str, int n, int &k, int m, int &min, char solu[])
 {
-    char buf[2 * n];
-    char buf1[2 * n];
+    char buf[n+k+1];
+    char buf1[n+k+1];
 
     buf[0] = str[0];
-    printPatternUtil(str, buf, 1, 1, n, k, m); //pustimo 0 na zacetku
+    printPatternUtil(str, buf, 1, 1, n, k, m, min, solu); //pustimo 0 na zacetku
 
     buf1[0] = '1';
-    printPatternUtil(str, buf1, 0, 1, n, k-1, m); //vrinemo 1 na zacetek
+    printPatternUtil(str, buf1, 0, 1, n, k-1, m, min, solu); //vrinemo 1 na zacetek
 }
 
 int main(int argc, char* argv[]) {
@@ -74,7 +108,21 @@ int main(int argc, char* argv[]) {
     file >> n >> k >> m;
     std::getline(file, line);
     file >> niz; // 0 - hruska, 1 - jabolko
-    printPattern(niz, n, k, m);
+
+    vector<int> insertedIndices;
+    insertedIndices.reserve(k);
+    int min = INT_MAX;
+    char solu[n+k+1];
+    printPattern(niz, n, k, m, min, solu);
+
+    cout << min << "\n";
+    findInsertedIndices(niz, solu, insertedIndices);
+    cout << insertedIndices.size();
+    for (int &i : insertedIndices){
+        cout << " " << i;
+    }
+    cout << "\n";
+    file.close();
     return 0;
 }
 
