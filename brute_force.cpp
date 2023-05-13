21c2df122b714350bc2be845f553c296d03fb62a
avalentincic <andraz.valentincic@student.um.si>
2023-05-13 13:31:51 +0200

    add all permutations

--- brute_force.cpp
+++ brute_force.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+void printPatternUtil(const string str, char buff[], int i, int j, int n, int k)
+{
+    if (i == n)
+    {
+        buff[j] = '\0';
+        cout << buff << endl;
+        return;
+    }
+
+    if (k <= 0) {
+        while (j < n){
+            buff[j] = str[i];
+            i++;
+            j++;
+        }
+        buff[j] = '\0';
+        cout << buff << endl;
+        return;
+    }
+
+    // Either put the character
+    buff[j] = str[i];
+    printPatternUtil(str, buff, i + 1, j + 1, n, k);
+
+    // Or put 1
+    buff[j] = '1';
+    //buff[j + 1] = str[i];
+    k--;
+    printPatternUtil(str, buff, i + 1, j + 1, n, k);
+}
+
+void printPattern(const string str, int n, int &k)
+{
+    char buf[2 * n];
+    char buf1[2 * n];
+
+    buf[0] = str[0];
+    printPatternUtil(str, buf, 1, 1, n, k); //pustimo 0 na zacetku
+
+    buf1[0] = '1';
+    printPatternUtil(str, buf1, 0, 1, n, k-1); //vrinemo 1 na zacetek
+}
 
 int main(int argc, char* argv[]) {
 
@@ -18,6 +62,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    int n, k, m; // n-jabolk, k-vrinjenih, m-to pobira
+    string niz;
+    file >> n >> k >> m;
+    std::getline(file, line);
+    file >> niz; // 0 - hruska, 1 - jabolko
+    printPattern(niz, n,k);
     return 0;
 }
 
