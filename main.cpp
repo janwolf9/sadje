b3c4e501d7d9ee1a0ae330ecda3550c9c1a6e664
wolfe <jan.wolf9@gmail.com>
2023-05-14 19:57:56 +0200

    between commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+
 int main(int argc, char **argv) {
     int maxN = 10000;
     int maxK = 50;
     int maxM = 10000;
-    int dp[maxK + 1][maxN + 1];
 
-    int n, k, m, c, a[maxN], r, rw[maxK];
-    // Read the input data.
+    int dp[maxK + 1][maxN + 1];
+    dp[0][0] = 0;
+
+    int n, k, m;
+    int c, r;
+    int input[maxN];
+    int indices[maxK];
+
     std::ifstream myFile;
     if (argc > 1) {
         myFile.open(argv[1]);
@@ -18,142 +24,87 @@ int main(int argc, char **argv) {
     } else {
         return 2;
     }
-    //FILE *f = (argc > 1) ? fopen(argv[1], "rt") : stdin;
-    myFile >> n;
-    myFile >> k;
-    myFile >> m;
 
-    if (1 > n) {
+    myFile >> n >> k >> m;
+
+    if (1 > n || n > maxN) {
         return 3;
     }
-    if (n > maxN) {
-        return 31;
-    }
-    if (0 > k) {
+    if (0 > k || k > maxK) {
         return 4;
     }
-    if (k > maxK) {
-        return 41;
-    }
-    if (1 > m) {
+    if (1 > m || m > maxM) {
         return 5;
     }
-    if (m > maxM) {
-        return 51;
-    }
 
-//#ifdef MODULO
-//    ok_ = fscanf(f, "%d %d", &n, &k); Assert(ok_ == 2);
-//	m = MODULO;
-//#else
-//    ok_ = fscanf(f, "%d %d %d", &n, &k, &m); Assert(ok_ == 3);
-//#endif
-//    Assert(1 <= n); Assert(n <= maxN);
-//    Assert(0 <= k); Assert(k <= maxK);
-//    Assert(1 <= m); Assert(m <= maxM);
-    do {
-        //c = fgetc(f);
+    int pos = 0;
+    do{
         c = myFile.get();
-    } while ((c == '\n' || c == '\r'));//&& c != EOF
-    //Assert(c != EOF);
-    if (c == EOF) {
-        return 6;
-    }
-    for (int i = 0; i < n; i++) {
-        //Assert(c == '0' || c == '1');
-        if (c != '1' && c != '0') {
-            return 7;
+        if (c == '1' || c == '0') {
+            input[pos] = c - '0';
+            pos++;
         }
-        a[i] = c - '0';
-        c = myFile.get();
-        //c = fgetc(f);
-    }
-    while (c == '\n' || c == '\r') {
-        c = myFile.get();
-    }
-    //Assert(c == EOF);
-    if (c != EOF) {
-        return 61;
-    }
-    //if (f != stdin) fclose(f);
+
+    }while(c != EOF);
+
     myFile.close();
-    // Solve the problem.  g[j][i] is the solution of the subproblem
-    // where we insert exactly j ones into the string a[0..i-1].
-    dp[0][0] = 0;
+
     for (int i = 1; i <= n; i++) {
         if (i % m == 0) {
-            dp[0][i] = dp[0][i - 1] + a[i - 1];
+            dp[0][i] = dp[0][i - 1] + input[i - 1];
         } else {
             dp[0][i] = dp[0][i - 1];
         }
-        //g[0][i] = g[0][i - 1] + (i % m == 0 ? a[i - 1] : 0);
     }
+
     for (int j = 1; j <= k; j++) {
         int tmp = j / m;
         dp[j][0] = tmp;
-        //g[j][0] = j / m;
         for (int i = 1; i <= n; i++) {
-            //g[j][i] = g[j - 1][i] + ((i + j) % m == 0 ? 1 : 0);
             if ((i + j) % m == 0) {
                 dp[j][i] = dp[j - 1][i] + 1;
+                c = dp[j][i - 1] + input[i - 1];
             } else {
                 dp[j][i] = dp[j - 1][i];
-            }
-            if ((i + j) % m == 0) {
-                c = dp[j][i - 1] + a[i - 1];
-            } else {
                 c = dp[j][i - 1];
             }
-            //c = g[j][i - 1] + ((i + j) % m == 0 ? a[i - 1] : 0);
+
             if (c < dp[j][i]) {
                 dp[j][i] = c;
             }
         }
     }
-    //if (false)
-    for (int j = 0; j <= k; j++) {
-        std::cout << "g[" << j << "] =";
-        //printf("g[%d] =", j);
-        for (int i = 0; i <= n; i++) {
-            std::cout << " " << dp[j][i];
-            //printf(" %d", g[j][i]);
-        }
-        //printf("\n");
-        std::cout << std::endl;
-    }
-    // Find the number of insertions which leads to the best solution.
+
     r = 0;
     for (int j = 1; j <= k; j++) {
         if (dp[j][n] < dp[r][n]) {
-            r = j; /* < finds the minimum number of insertions leading to the best solution, <= finds the maximum */
+            r = j;
         }
     }
+
     int j = r;
     int i = n;
     while (j > 0) {
-        //Assert(i >= 0);
+
         if (i < 0) {
             return 8;
         }
+
         if ((i + j) % m == 0) {
             if (dp[j][i] == dp[j - 1][i] + 1) {
-                rw[--j] = i;
+                indices[--j] = i;
             } else {
                 --i;
             }
         } else {
             if (dp[j][i] == dp[j - 1][i]) {
-                rw[--j] = i;
+                indices[--j] = i;
             } else {
                 --i;
             }
         }
-
-//        if (g[j][i] == g[j - 1][i] + ((i + j) % m == 0 ? 1 : 0)) //i == 0 ||
-//            rw[--j] = i;
-//        else --i;
     }
-    // Output the results.
+
     std::ofstream myOutputFile;
     if (argc > 2) {
         myOutputFile.open(argv[2]);
@@ -163,12 +114,15 @@ int main(int argc, char **argv) {
     } else {
         return 21;
     }
+
     std::cout << dp[r][n] << "\n" << r;
     myOutputFile << dp[r][n] << "\n" << r;
+
     for (j = 0; j < r; j++) {
-        myOutputFile << " " << rw[j] + j + 1;
-        std::cout << " " << rw[j] + j + 1;
+        myOutputFile << " " << indices[j] + j + 1;
+        std::cout << " " << indices[j] + j + 1;
     }
+
     myOutputFile << "\n";
     myOutputFile.close();
     return 0;
